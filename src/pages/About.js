17be2998348c2b9c87c29ec75a1bebb1f5b0b6db
sean3434 <seannyc34@gmail.com>
2022-05-11 14:52:32 -0400

    Portfolio Frontend Running

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,17 +5,26 @@ function About(props) {
   const [about, setAbout] = useState(null);
 
   // create function to make api call
-  const getAboutData = async () => {
-    // make api call and get response
-    const response = await fetch(props.URL + "about");
-    // turn response into javascript object
-    const data = await response.json();
-    // set the about state to the data
-    setAbout(data);
-  };
+//   const getAboutData = async () => {
+//     // make api call and get response
+//     const response = await fetch(props.URL + "about");
+//     // turn response into javascript object
+//     const data = await response.json();
+//     // set the about state to the data
+//     setAbout(data);
+//   };
 
+useEffect(()=> {
+    const getAbout = async () => {
+        const response = await fetch(props.URL + "about");
+        const data = await response.json();
+        console.log(data)
+        setAbout(data)
+    };
+    getAbout()}, [props.URL]);
+  
   // make an initial call for the data inside a useEffect, so it only happens once on component load
-  useEffect(() => getAboutData(), []);
+//   useEffect(() => getAboutData(), []);
 
   // define a function that will return the JSX needed once we get the data
   const loaded = () => (
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -4,25 +4,34 @@ function Projects(props) {
   // create state to hold projects
   const [projects, setProjects] = useState(null);
 
-  //create function to make api call
-  const getProjectsData = async () => {
-    //make api call and get response
-    const response = await fetch(props.URL + "projects");
-    // turn response into javascript object
-    const data = await response.json();
-    // set the projects state to the data
-    setProjects(data);
-  };
+//   //create function to make api call
+//   const getProjectsData = async () => {
+//     //make api call and get response
+//     const response = await fetch(props.URL + "projects");
+//     // turn response into javascript object
+//     const data = await response.json();
+//     // set the projects state to the data
+//     setProjects(data);
+//   };
+
+  useEffect(()=> {
+    const getProjects = async () => {
+        const response = await fetch(props.URL + "projects");
+        const data = await response.json();
+        console.log(data)
+        setProjects(data)
+    };
+    getProjects()}, [props.URL]);
 
   // make an initial call for the data inside a useEffect, so it only happens once on component load
-  useEffect(() => getProjectsData(), []);
+//   useEffect(() => getProjectsData(), []);
 
   // define a function that will return the JSX needed once we get the data
   const loaded = () => {
     return projects.map((project) => (
       <div>
         <h1>{project.name}</h1>
-        <img src={project.image} />
+        <img src={project.image} alt={project.name}/>
         <a href={project.git}>
           <button>Github</button>
         </a>
